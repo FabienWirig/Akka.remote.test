1aa95c58ab124ab0d3621e97ed204568ba602e12
FabienW <fabien.wirig@gmail.com>
2017-02-16 22:56:01 +0100

    Akka remote deployment on console app

---
+++ WinTail.DeployTarget.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Configuration;
+
+namespace Wintail.DeployTarget.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var system = ActorSystem.Create("DeployTargetConsoleApp"))
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
